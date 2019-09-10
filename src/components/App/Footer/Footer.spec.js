861fb4c35b0a68e8168208a00c741e1d95298fb1
Dan Hedgecock <hedgecock.d@gmail.com>
2019-09-10 07:20:51 -0700

    New: Add Emotion styling 👩‍🎤

--- src/components/App/Footer/Footer.spec.js
+++ src/components/App/Footer/Footer.spec.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { render } from '@testing-library/react'
 
 import Footer from './Footer'
+import { emotionWrapper } from '@/theme/emotion'
 
 describe('<Footer />', () => {
   test('Footer snapshot', () => {
-    const { container } = render(<Footer />)
+    const { container } = render(emotionWrapper(<Footer />))
     expect(container.firstChild).toMatchSnapshot()
   })
 })
