01811034ba3e7ee9478fe34e6b4ca6e04199c7ce
Dan Hedgecock <hedgecock.d@gmail.com>
2019-09-26 06:07:06 -0700

    Chore: Deps 🆙 and Router hooks 🎉

--- src/components/universal/ScrollToTop/ScrollToTop.js
+++ src/components/universal/ScrollToTop/ScrollToTop.js
@@ -1,28 +1,17 @@
 import { useEffect } from 'react'
-import { node, shape, string } from 'prop-types'
-import { withRouter } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 /**
  * Component handles scrolling the application back to top of window after every
  * route change. It should be included only once per app.
  * Ref: https://reacttraining.com/react-router/web/guides/scroll-restoration
  */
-const ScrollToTop = ({ location, children }) => {
+export default function ScrollToTop() {
+  const location = useLocation()
+
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [location.pathname])
 
-  return children || null
+  return null
 }
-
-ScrollToTop.propTypes = {
-  children: node,
-  location: shape({
-    pathname: string.isRequired,
-  }).isRequired,
-}
-ScrollToTop.defaultProps = {
-  children: null,
-}
-
-export default withRouter(ScrollToTop)
