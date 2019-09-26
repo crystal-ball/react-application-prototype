f9f2ffa012a3e041349b0a8054b7b4c1dd32ea03
Dan Hedgecock <Hedgecock.D@gmail.com>
2019-09-26 07:01:03 -0700

    Test: Adding tests to app ✅ (#29)

---
+++ src/dux/app/app.spec.js
@@ -0,0 +1,29 @@
+import reducer, { selectRad, dispatchRadToggled } from './index'
+
+const mockState = () => ({
+  app: {
+    rad: true,
+  },
+})
+
+describe('app dux', () => {
+  // --------------------------------------------------------
+  // Reducer
+  test('When reducer is called with toggle action, the rad state is toggled', () => {
+    const newState = reducer(mockState().app, dispatchRadToggled())
+    expect(newState.rad).toEqual(false)
+  })
+
+  // --------------------------------------------------------
+  // Selectors
+  test('When selectRad is called, then the current rad state is returned', () => {
+    expect(selectRad(mockState())).toEqual(true)
+  })
+
+  // --------------------------------------------------------
+  // Action creators
+
+  test('When dispatchRadToggled is called, then an action object is returned', () => {
+    expect(dispatchRadToggled()).toEqual({ type: 'APP/RAD_TOGGLED' })
+  })
+})
