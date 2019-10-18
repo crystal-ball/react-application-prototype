bdf4d70f572a80d06c66aa623548961c3e0e3bd0
Dan Hedgecock <Hedgecock.D@gmail.com>
2019-10-17 18:50:35 -0700

    Update: Dependencies and Zeit 🆙 (#48)

--- babel.config.js
+++ babel.config.js
@@ -80,8 +80,9 @@ module.exports = function babelConfigs(api) {
 
       // --- Additional stage-3 proposals not present in preset-env set
       '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
       '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-private-methods',
     ],
   }
 }
