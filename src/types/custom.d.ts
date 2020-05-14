e0cc33d8b784d53e3d66d41360b9b8556129ffa6
Dan Hedgecock <hedgecock.d@gmail.com>
2020-05-13 17:03:44 -0700

    Chore: Fix custom types for webpack loader modules ⚙️

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,5 +1,17 @@
+/* eslint-disable */
 /** webpack SVG imports */
+
 declare module '*.svg' {
-  const svg: string
-  export default svg
+  const icon: React.FunctionComponent<{ className?: string }>
+  export default icon
+}
+
+declare module '*.jpg' {
+  const jpg: string
+  export default jpg
+}
+
+declare module '*.scss' {
+  const styles: { [key: string]: string }
+  export default styles
 }
