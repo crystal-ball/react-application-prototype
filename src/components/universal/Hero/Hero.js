fe6e3bcc8afc8c9dd391ea917cbd7fa1e2db1dd6
Dan Hedgecock <hedgecock.d@gmail.com>
2020-05-13 17:03:11 -0700

    Update: Hero styles to CSS modules 💖

--- src/components/universal/Hero/Hero.js
+++ src/components/universal/Hero/Hero.js
@@ -1,47 +1,19 @@
 import React from 'react'
 import { Flex, Typography } from 'componentry'
-import { css } from '@emotion/core'
 
 import Github from '@/media/github.svg'
-import radpackBg from './radpack-bg.jpg'
-
-const leftContainerStyles = css`
-  width: 40%;
-  background-image: url(${radpackBg});
-  background-position: center center;
-  position: sticky;
-`
-
-const stickyTitleContainerStyles = css`
-  position: sticky;
-  top: 3rem;
-  padding-top: 3rem;
-`
-
-const titleStyles = css`
-  font-family: 'Alex Brush', cursive;
-  font-size: 6rem;
-  font-weight: 300;
-  line-height: 1.25;
-  color: #a8ffdb;
-  text-shadow: #f70777 0px 0 15px;
-`
-
-// TODO: use theme to set fontSize to an h1
-const iconStyles = css`
-  fill: #a8ffdb !important;
-`
+import classes from './hero.scss'
 
 export default function Hero() {
   return (
-    <div css={leftContainerStyles} data-testid='hero-img'>
+    <div className={classes.container} data-testid='hero-img'>
       <Flex
         align='center'
-        css={stickyTitleContainerStyles}
         direction='column'
         justify='center'
+        className={classes.titleContainer}
       >
-        <Typography variant='title' align='center' css={titleStyles}>
+        <Typography variant='title' align='center' className={classes.title}>
           The Order
           <br />
           of the
@@ -54,7 +26,7 @@ export default function Hero() {
             data-testid='github-anchor'
             href='https://github.com/crystal-ball'
           >
-            <Github className='icon' css={iconStyles} />
+            <Github className={`icon ${classes.icon}`} />
           </a>
         </div>
       </Flex>
