129a4ea03a73bffbf7f90c045fb8280e92ee3294
Dan Hedgecock <hedgecock.d@gmail.com>
2020-05-31 09:10:30 -0700

    Chore: awesomeness

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
 
 const eloquence = require('eslint-config-eloquence')
 
-const configs = eloquence({ target: 'react' })
-configs.ignorePatterns = ['!.*', 'public/*']
-
-module.exports = configs
+module.exports = eloquence({
+  target: 'react',
+  ignorePatterns: ['!.*', 'public/*'],
+})
