c551faa576db2e8528b8e17008f1ca32cf203edd
Dan Hedgecock <hedgecock.d@gmail.com>
2020-09-05 14:05:12 -0700

    Fix: Tests

--- src/components/HomeScreen/HomeScreen.spec.js
+++ src/components/HomeScreen/HomeScreen.spec.js
@@ -12,6 +12,6 @@ describe('<HomeScreen />', () => {
         <HomeScreen />
       </Provider>,
     )
-    expect(screen.getByText(/Prototype React application/))
+    expect(screen.getByText(/Prototype application/))
   })
 })
