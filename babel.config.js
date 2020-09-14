c50a2a32473e6f77492c3c940bba40b972197018
Dan Hedgecock <hedgecock.d@gmail.com>
2020-09-14 06:25:49 -0700

    Chore: start command

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,10 @@ const babelBase = require('@crystal-ball/babel-base').default
 module.exports = function babelConfigs(api) {
   const configs = babelBase({ env: api.env, target: 'react' })
 
-  configs.env.cypress = {
+  /**
+   * Target environment for collecting code coverage against the application
+   */
+  configs.env.instrument = {
     plugins: ['istanbul'],
   }
 
