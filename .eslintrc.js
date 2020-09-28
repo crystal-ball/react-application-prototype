f2e02ae27f14aee0b46e756025b734674a74c096
Dan Hedgecock <hedgecock.d@gmail.com>
2020-09-27 18:05:08 -0700

    Chore: Update ESLint and deps 🆙

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ const eloquence = require('eslint-config-eloquence')
 
 module.exports = eloquence({
   target: 'react',
+  enableMDX: true,
   rules: {
     // Enforce that testing library is used through utils/testing-library only
     'no-restricted-imports': ['error', { paths: ['@testing-library/react'] }],
