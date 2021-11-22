fd1c382d3e201b80877410c84e4871b5b3e68bb3
Dan Hedgecock <Hedgecock.D@gmail.com>
2021-11-22 08:19:09 -0800

    Update: Tracing dependencies (#338)

--- src/utils/setup-tracer.ts
+++ src/utils/setup-tracer.ts
@@ -1,36 +1,33 @@
-import opentelemetry from '@opentelemetry/api' // eslint-disable-line -- Import required to get a tracer in application
 import { ZoneContextManager } from '@opentelemetry/context-zone'
 import { CollectorTraceExporter } from '@opentelemetry/exporter-collector'
 import { registerInstrumentations } from '@opentelemetry/instrumentation'
 import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load'
 import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch'
 import { Resource } from '@opentelemetry/resources'
-import { BatchSpanProcessor } from '@opentelemetry/tracing'
-import { WebTracerProvider } from '@opentelemetry/web'
+import {
+  BatchSpanProcessor,
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from '@opentelemetry/sdk-trace-base'
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web'
 
 import { LS_ACCESS_TOKEN, NODE_ENV, RELEASE_VERSION } from '@/config/environment'
 
+// --- PROVDER
+
+// Create the provider for activating and tracking spans
+const tracerProvider = new WebTracerProvider({
+  // Include a service.version attribute in all spans to enable Lightstep deployment markers
+  resource: new Resource({
+    'service.name': 'react-application-prototype',
+    'service.version': RELEASE_VERSION ?? 'local',
+  }),
+})
+
+// --- PROCESSOR/EXPORTER
+
 if (NODE_ENV === 'production') {
-  // Create a provider for activating and tracking spans
-  const tracerProvider = new WebTracerProvider({
-    // Include a service.version attribute in all spans to enable Lightstep deployment markers
-    resource: new Resource({
-      'service.name': 'react-application-prototype',
-      'service.version': RELEASE_VERSION,
-    }),
-  })
-
-  registerInstrumentations({
-    instrumentations: [
-      new DocumentLoadInstrumentation(),
-      new FetchInstrumentation({
-        ignoreUrls: [/localhost:8090\/sockjs-node/],
-      }),
-    ],
-    tracerProvider,
-  })
-
-  // Connect to Lightsstep by configuring the exporter with your endpoint and access token.
+  // Connect to Lightstep by configuring the exporter with your endpoint and access token.
   tracerProvider.addSpanProcessor(
     new BatchSpanProcessor(
       new CollectorTraceExporter({
@@ -44,9 +41,27 @@ if (NODE_ENV === 'production') {
       },
     ),
   )
-
-  // Register the tracer
-  tracerProvider.register({
-    contextManager: new ZoneContextManager(),
-  })
+} else {
+  // Use console exporter in dev
+  tracerProvider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()))
 }
+
+// --- INSTRUMENTATION
+
+registerInstrumentations({
+  instrumentations: [
+    new DocumentLoadInstrumentation(),
+    new FetchInstrumentation({
+      ignoreUrls: [/localhost:8090\/sockjs-node/],
+    }),
+  ],
+  tracerProvider,
+})
+
+// --- REGISTER
+
+// Register with config for zone and propagators
+tracerProvider.register({
+  contextManager: new ZoneContextManager(),
+  // propagator
+})
